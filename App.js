260f2fe1adf11d403fa4636fdeed58eb259315a5
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-04-19 21:02:52 +0200

    v 1.1
    - fixed data loading on startup
    - styling

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import { Cards, Chart, CountryPicker } from './components';
 import { fetchData } from './api/index';
+import './App.css';
 
 const App = () => {
-  const [ data, setData ] = useState([])
+  const [ data, setData ] = useState([''])
   const [ country, setCountry ] = useState('')
  
-  
+const startUpfetch = async () =>{
+  const response = fetchData()
+  const data = await response
+  setData(data)
+}
  const  handleCountryChange = async (country) =>{
     const fetchedData = await fetchData(country)
     setData(fetchedData)
@@ -15,12 +19,12 @@ const App = () => {
   };
 
 useEffect(() => {
-  const fetchedData = fetchData()
-    setData(fetchedData)
+  startUpfetch() 
 }, [])
 
   return (
   <div className='container'>
+    <img className='imgLogo' src={'https://lh3.googleusercontent.com/proxy/NRHZSkmmJF2CMWcM1Z9khWHSin8737viReQ_L7CSGozkj9jjWCNMFB4qOn7UpH7wutVanS4kAxjDScn4ItF5Rxi-RhOumMYyiK6v3SHJ0XZy_4bavtVS2vwWKA'}></img>
     <Cards data={data}/>
     <CountryPicker handleCountryChange={handleCountryChange}/>
     <Chart data={data} country={country}/>
